8955ac5c5bcdf4864816427b3a8d8dfb8035911b
trganda <jongsx1203@gmail.com>
2026-04-09 17:26:59 +0800

    feat: :fire: rm useless arguments

--- cmd/test/run.go
+++ cmd/test/run.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log/slog"
-	"os"
 
 	"github.com/spf13/cobra"
 
@@ -18,7 +17,6 @@ import (
 func newRunUnitTestsCmd(base *string, useBundle *bool) *cobra.Command {
 	var (
 		numThreads int
-		workDir    string
 		lang       string
 		codeqlArgs string
 	)
@@ -28,11 +26,10 @@ func newRunUnitTestsCmd(base *string, useBundle *bool) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			slog.Debug("Executing test run execute command",
 				"language", lang, "threads", numThreads)
-			return runExecuteUnitTests(*base, lang, workDir, codeqlArgs, numThreads, *useBundle)
+			return runExecuteUnitTests(*base, lang, codeqlArgs, numThreads, *useBundle)
 		},
 	}
 	cmd.Flags().IntVar(&numThreads, "num-threads", 4, "Number of threads for test execution")
-	cmd.Flags().StringVar(&workDir, "work-dir", os.TempDir(), "Directory for intermediate output files")
 	cmd.Flags().StringVar(&lang, "language", "", "Language to run tests for")
 	cmd.Flags().StringVar(&codeqlArgs, "codeql-args", "", "Extra arguments to pass to CodeQL")
 	_ = cmd.MarkFlagRequired("num-threads")
@@ -41,7 +38,7 @@ func newRunUnitTestsCmd(base *string, useBundle *bool) *cobra.Command {
 	return cmd
 }
 
-func runExecuteUnitTests(base, lang, workDir, codeqlArgs string, numThreads int, useBundle bool) error {
+func runExecuteUnitTests(base, lang, codeqlArgs string, numThreads int, useBundle bool) error {
 	slog.Debug("Running unit tests", "base", base, "lang", lang, "threads", numThreads, "use-bundle", useBundle)
 	cfg, err := config.MustLoadFromFile(base)
 	if err != nil {
@@ -52,7 +49,6 @@ func runExecuteUnitTests(base, lang, workDir, codeqlArgs string, numThreads int,
 		"language", lang,
 		"codeql-cli", cfg.CodeQLCLI,
 		"threads", numThreads,
-		"work-dir", workDir,
 		"codeql-args", codeqlArgs,
 		"use-bundle", useBundle,
 	)
@@ -64,7 +60,7 @@ func runExecuteUnitTests(base, lang, workDir, codeqlArgs string, numThreads int,
 
 	slog.Debug("Using CodeQL binary", "path", codeql)
 
-	args := []string{"resolve", "test", "--format", "json", fmt.Sprintf("%s/%s", base, language.ToDirectory(lang))}
+	args := []string{"resolve", "tests", "--format", "json", fmt.Sprintf("%s/%s", base, language.ToDirectory(lang))}
 	runner := executil.NewRunner(codeql)
 	res, err := runner.Run(args...)
 	if err != nil {
@@ -78,7 +74,26 @@ func runExecuteUnitTests(base, lang, workDir, codeqlArgs string, numThreads int,
 
 	slog.Info("Resolved test files", "count", len(resolvedTests))
 
-	// args = []string{"test", "run", "--language", lang, "--threads", string(numThreads), "--work-dir", workDir}
+	for _, testFile := range resolvedTests {
+		slog.Debug("Running test file", "file", testFile)
+		testArgs := []string{"test", "run", "--threads", fmt.Sprintf("%d", numThreads)}
+		if codeqlArgs != "" {
+			testArgs = append(testArgs, codeqlArgs)
+		}
+		testArgs = append(testArgs, testFile)
+		res, err := runner.Run(testArgs...)
+		if err != nil {
+			if res != nil && len(res.Stderr) > 0 {
+				slog.Error("Test command stderr result", "output", res.StderrString())
+			}
+			return fmt.Errorf("failed to run test %s: %w", testFile, err)
+		}
+		if res != nil && len(res.Stdout) > 0 {
+			slog.Debug("Test command stdout result", "output", res.StdoutString())
+		}
+	}
+
+	slog.Info("Completed execution of all unit tests")
 
 	return nil
 }
